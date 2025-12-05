5a127c9bc26877be9de2f3c94dc331c8be283617
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-12-04 16:34:56 -0800

    add nvim-autopairs

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ lzn.load({
   require("plugins.conform"),
   require("plugins.neo-tree"),
   require("plugins.nui"),
+  require("plugins.nvim-autopairs"),
   require("plugins.nvim-treesitter"),
   require("plugins.nvim-web-devicons"),
   require("plugins.plenary"),
