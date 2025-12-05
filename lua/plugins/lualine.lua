bf22257d1ec3cf1fb9a9530fa98f7c8e5b3d985d
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-12-04 16:59:03 -0800

    add neo-tree extension to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,10 @@ return {
   "lualine.nvim",
   event = "DeferredUIEnter",
   after = function()
-    require("lualine").setup({})
+    require("lualine").setup({
+      extensions = {
+        "neo-tree",
+      },
+    })
   end,
 }
