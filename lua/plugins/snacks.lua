c31ba690a5fbfeca282490528c05887c15b46675
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-12-25 22:30:57 -0800

    add snacks conf

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,6 +19,42 @@ return {
         scope = { enabled = false },
       },
       notifier = { enabled = true },
+      picker = {
+        enabled = true,
+        sources = {
+          files = {
+            layout = { fullscreen = true },
+          },
+          grep = {
+            layout = { fullscreen = true },
+          },
+        },
+        ui_select = true,
+      },
+      scroll = { enabled = true },
+      words = { enabled = true },
+    })
+
+    require("which-key").add({
+      {
+        "<leader>f",
+        desc = "+[f]ind(snacks)",
+        icon = " ",
+      },
+      {
+        "<leader>ff",
+        function()
+          Snacks.picker.files()
+        end,
+        desc = "[f]iles",
+      },
+      {
+        "<leader>fg",
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = "[g]rep",
+      },
     })
   end,
 }
