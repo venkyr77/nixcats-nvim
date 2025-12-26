29cde1715ba5ef89bc5f28881ac09e313c76c708
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-12-26 00:26:48 -0800

    add treesitter conf

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,9 +3,16 @@ return {
   lazy = false,
   after = function()
     require("nvim-treesitter.configs").setup({
+      auto_install = false,
+      ensure_installed = {},
       highlight = {
+        additional_vim_regex_highlighting = false,
         enable = true,
       },
+      indent = {
+        enable = true,
+      },
+      sync_install = false,
     })
   end,
 }
