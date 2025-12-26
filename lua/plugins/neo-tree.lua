be5ea101ada121f4af438c5f5831e532deb85513
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-12-25 23:08:06 -0800

    add neotree conf

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,6 +9,36 @@ return {
     lzn.trigger_load("plenary.nvim")
   end,
   after = function()
-    require("neo-tree").setup({})
+    require("neo-tree").setup({
+      default_component_configs = {
+        indent = {
+          with_expanders = true,
+        },
+      },
+      enable_cursor_hijack = true,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          never_show = {
+            ".git",
+          },
+          visible = true,
+        },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+        group_empty_dirs = true,
+        scan_mode = "deep",
+      },
+    })
+
+    require("which-key").add({
+      {
+        "\\",
+        ":Neotree toggle<CR>",
+        desc = "[N]eotree [t]oggle",
+      },
+    })
   end,
 }
