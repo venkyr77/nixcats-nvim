6180c0e0759e2e1993b8832ab10f11f028006027
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-12-25 22:08:36 -0800

    formatting

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,6 +2,8 @@ return {
   "auto-save.nvim",
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
   after = function()
-    require("auto-save").setup({ debounce_delay = 100 })
+    require("auto-save").setup({
+      debounce_delay = 100,
+    })
   end,
 }
